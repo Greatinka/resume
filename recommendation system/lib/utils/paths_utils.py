d763a0acfe27ceedb961b07743c06a9e0896189e
Greatinka <111311434+Greatinka@users.noreply.github.com>
2026-02-26 13:26:00 +0300

    Update paths_utils.py

--- recommendation system/lib/utils/paths_utils.py
+++ recommendation system/lib/utils/paths_utils.py
@@ -2,17 +2,8 @@ from config import IS_LMS_ENV, MODEL_PATH_LMS
 
 
 def get_model_path(path: str) -> str:
-    """
-    Determine the model file path based on the environment.
+    """Determine the model file path based on the environment"""
     
-    Adapts paths for both local environment and work environment (LMS).
-    
-    Args:
-        path: Default model path for local environment
-        
-    Returns:
-        Correct model path based on current environment
-    """
     if IS_LMS_ENV:
         return MODEL_PATH_LMS
     return path
